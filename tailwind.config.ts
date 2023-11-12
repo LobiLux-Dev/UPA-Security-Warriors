e5845c4a827dded7d2a6e281ec768fe98df6c4cf
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-12 14:10:07 -0600

    Rearrange dependencies and add DaisyUI theme.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config as TailwindCSSConfig } from 'tailwindcss'
 
-const config: Config = {
-	content: [
-		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
-		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
-	],
+import DaisyUI, { type Config as DaisyUIConfig } from 'daisyui'
+
+const config = {
+	content: ['./src/pages/**/*.{ts,tsx,mdx}', './src/components/**/*.{ts,tsx,mdx}', './src/app/**/*.{ts,tsx,mdx}'],
+	daisyui: {
+		themes: [
+			{
+				default: {
+					primary: '#FF6079',
+					secondary: '#FE3051',
+					accent: '#111111',
+					neutral: '#D8D8D8', // Improvised
+					'base-100': '#0A0A0A',
+					info: '#A1B0BF',
+					success: '#00CC66', // Improvised
+					warning: '#FFD166', // Improvised
+					error: '#FF5858', // Improvised
+				},
+			},
+		],
+	} satisfies DaisyUIConfig,
+	plugins: [DaisyUI],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
-}
+} satisfies TailwindCSSConfig
 
 export default config
