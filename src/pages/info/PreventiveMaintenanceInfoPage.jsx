d5131393d98d0a700a605b52474ee3e9fe59f7ec
MVNdev <114366014+MVNdev@users.noreply.github.com>
2023-11-14 22:39:58 -0600

    Add content to PreventiveMaintenanceInfoPage and RealCasesInfoPage

--- src/pages/info/PreventiveMaintenanceInfoPage.jsx
+++ src/pages/info/PreventiveMaintenanceInfoPage.jsx
@@ -1,3 +1,69 @@
 export const PreventiveMaintenanceInfoPage = () => {
-	return <></>
+	return (
+		<>
+			<h1>Mantenimiento</h1>
+
+			<p>
+				<span>Mantenimiento correctivo de software: </span>El mantenimiento correctivo del software es la forma clásica
+				y típica de mantenimiento (para el software y cualquier otra cosa). El mantenimiento de software correctivo es
+				necesario cuando algo sale mal en una pieza de software, incluidos fallos y errores. Estos pueden tener un
+				impacto generalizado en la funcionalidad del software en general y, por lo tanto, deben abordarse lo antes
+				posible.
+			</p>
+			<p>
+				<span>Mantenimiento preventivo de software: </span>El mantenimiento preventivo de software está mirando hacia el
+				futuro para que su software pueda seguir funcionando como se desee durante el mayor tiempo posible. Esto incluye
+				realizar los cambios necesarios, actualizaciones, adaptaciones y más. El mantenimiento preventivo del software
+				puede abordar pequeños problemas que en un momento dado pueden carecer de importancia, pero pueden convertirse
+				en problemas mayores en el futuro. Estos se denominan fallas latentes que deben detectarse y corregirse para
+				asegurarse de que no se conviertan en fallas efectivas.
+			</p>
+			<p>
+				<span>Mantenimiento perfectivo de software: </span>El mantenimiento perfectivo de software tiene como objetivo
+				ajustar el software agregando nuevas características según sea necesario y eliminando características que son
+				irrelevantes o no efectivas en el software dado. Este proceso mantiene el software relevante a medida que el
+				mercado y las necesidades del usuario cambian.
+			</p>
+			<p>
+				<span>Mantenimiento adaptativo de software: </span>El Mantenimiento adaptativo de software tiene que ver con las
+				tecnologías cambiantes, así como con las políticas y reglas relacionadas con su software. Las cuales incluyen
+				cambios en el sistema operativo, almacenamiento en la nube, hardware, etc. Cuando se realizan estos cambios, su
+				software debe adaptarse para cumplir adecuadamente los nuevos requisitos y continuar funcionando bien.
+			</p>
+			<p>
+				<span>¿Que es el software? </span>El software es un conjunto de programas, instrucciones y datos que controlan
+				el funcionamiento de una computadora o dispositivo electrónico. Es la parte no tangible de un sistema
+				informático y se encarga de ejecutar tareas y aplicaciones, como procesamiento de datos, gestión de hardware y
+				proporcionar funcionalidad a los usuarios. El software se divide generalmente en dos categorías: software de
+				sistema (que controla el hardware) y software de aplicación (que realiza tareas específicas para el usuario)
+			</p>
+			<p>
+				<span>¿Que es el hardware? </span>El hardware esta compuesto de componentes que hacen el funcionamiento de un
+				equipo que empieza desde la tarjeta madre que es la que hace que todos los componentes se puedan conectar, como
+				por ejemplo esta formada por un procesador, memoria de disco duro, memoria ram, memoria ssd o m.2y opcionalmente
+				una tarjeta grafica por separado a los gráficos integrados del procesador.
+			</p>
+			<p>
+				<span>Mantenimiento a equipos de trabajo: </span>Es una serie de tareas y procedimientos que consisten en la
+				inspección de anomalías, limpieza y sustitución de piezas, materiales y sistemas usados, que ayuden a evitar
+				fallos en el equipo de cómputo y garantizar que se encuentran funcionando de acuerdo a sus capacidades.
+			</p>
+			<p>
+				<span>Mantenimiento del hardware: </span>El mantenimiento del hardware se trata principalmente de estar
+				limpiando constantemente el polvo generado por el uso de la computadora, también por eso se procura tener
+				apagado el equipo cuando no este usándose. También por otro lado el procesador después de mucho uso se tiene que
+				cambiar la pasta térmica ya que este mismo se eleva a mucha temperatura cada que se sobrecarga y necesita su
+				mantenimiento cada 2 meses dependiendo del uso que le des al equipo.
+			</p>
+
+			<span>Ventajas de tener un buen plan de mantenimiento:</span>
+
+			<ul>
+				<li>Alargas la vida útil de tu equipo</li>
+				<li>Previenes ataques de virus cibernéticos</li>
+				<li>Tu equipo será rápido al ejecutar programas</li>
+				<li>No tendrás sobre calentamiento</li>
+			</ul>
+		</>
+	)
 }
