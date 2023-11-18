34e9a8159c24cb3c4266e9d34dbc4dde63c0c269
MVNdev <114366014+MVNdev@users.noreply.github.com>
2023-11-17 20:56:28 -0600

    Add components and content for Avast and Kaspersky pages

--- src/pages/info/Avast.jsx
+++ src/pages/info/Avast.jsx
@@ -1,3 +1,72 @@
+import { HeaderTitle } from '../../components/HeaderTitle'
+import { NavBar } from '../../components/NavBar'
+import Card from '../mantenimiento/Card'
+
 export function Avast() {
-	return <div>Avast</div>
+	return (
+		<div className="bg-black">
+			<NavBar />
+			<HeaderTitle title={'Avast'} />
+			<div className="flex flex-col w-3/5 mx-auto items-center gap-5 mt-10">
+				<h2 className="text-xl font-bold">Avast Antivirus!: Protegiendo tu mundo digital</h2>
+				<p className="text-justify">
+					En un mundo cada vez más interconectado, la seguridad informática se ha convertido en una prioridad
+					fundamental. Con la proliferación de amenazas cibernéticas, desde virus y malware hasta ataques de phishing,
+					contar con un sólido software antivirus es esencial para salvaguardar nuestros dispositivos y datos. Avast
+					Antivirus se destaca como una opción popular y confiable en esta lucha constante contra las amenazas
+					digitales.
+				</p>
+				<p>Historia y Desarrollo</p>
+				<p className="text-justify">
+					Avast, fundada en 1988 por Pavel Baudis y Eduard Kucera en la República Checa, ha evolucionado a lo largo de
+					los años para convertirse en uno de los líderes en la industria de la seguridad cibernética. Comenzando como
+					un proyecto de investigación, la compañía ha crecido hasta ofrecer una gama completa de soluciones de
+					seguridad, con el antivirus Avast siendo su producto estrella.
+				</p>
+
+				<p>Interfaz de Usuario Intuitiva</p>
+				<p className="text-justify">
+					La interfaz de Avast es conocida por su diseño amigable e intuitivo. Incluso para aquellos que no son expertos
+					en tecnología, la navegación por las funciones y la comprensión de los informes de seguridad son tareas
+					sencillas.
+				</p>
+
+				<p>Versiones Gratuita y de Pago</p>
+				<p className="text-justify">
+					Avast ofrece una versión gratuita que proporciona una sólida protección contra malware básico. Para aquellos
+					que buscan funciones adicionales, como un cortafuegos más avanzado, protección contra ransomware y opciones de
+					privacidad mejoradas, la versión de pago, Avast Premium Security, podría ser la elección adecuada.
+				</p>
+
+				<p className="text-2xl font-bold">
+					Conoce mas de <span className="text-[#FE3051]">Avast</span>
+				</p>
+				<p>Conoce las ventajas, desventajas y el porque elegir Avast y descubrela</p>
+				<div className="flex container justify-between gap-5">
+					<Card
+						title={'Ventajas de Avast'}
+						description={
+							'Detecta y bloquea de virus y otro software malicioso, encuentra puntos débiles en la seguridad de la Wi-Fi, impide que los hackers cifren fotos y archivos personales, protege tus contraseñas, evita los sitios falsificados y ejecuta de manera segura programas sospechosos.'
+						}
+					/>
+					<Card
+						title={'Desventajas de Avast'}
+						description={
+							'Escaso nivel de protección, ralentización de equipos, es muy invasivo, bloquea aplicaciones legítimas (nuestro caso) y daña y elimina archivos del sistema.'
+						}
+					/>
+					<Card
+						title={'¿Porque elegir Avast?'}
+						description={
+							'Avast Antivirus es una opción sólida como mejor antivirus que brinda protección integral y personalizada contra malware, respaldada por un conjunto de funciones avanzadas y opciones de suscripción flexibles que se adaptan a sus necesidades de seguridad en línea.'
+						}
+					/>
+				</div>
+
+				<a href="https://acortar.link/8244Kh" target="blank" className="btn bg-[#FE3051] w-full my-10">
+					Comprar
+				</a>
+			</div>
+		</div>
+	)
 }
--- src/pages/info/Karspersky.jsx
+++ src/pages/info/Karspersky.jsx
@@ -1,3 +1,62 @@
+import { HeaderTitle } from '../../components/HeaderTitle'
+import { NavBar } from '../../components/NavBar'
+import Card from '../mantenimiento/Card'
+
 export function Karspersky() {
-	return <div>Karspersky</div>
+	return (
+		<div className="bg-black">
+			<NavBar />
+			<HeaderTitle title={'Karspersky'} />
+			<div className="flex flex-col w-3/5 mx-auto items-center gap-5 mt-10">
+				<h2 className="text-xl font-bold">Kaspersky Antivirus: Protección Avanzada para tu Ciberseguridad</h2>
+				<p className="text-justify">
+					En un mundo cada vez más interconectado y dependiente de la tecnología, la seguridad cibernética se ha
+					convertido en una preocupación crítica para individuos y empresas por igual. Las amenazas en línea, que van
+					desde virus y malware hasta ataques de phishing y ransomware, pueden poner en peligro la privacidad y la
+					integridad de los datos. Kaspersky Antivirus, desarrollado por la respetada empresa rusa Kaspersky Lab, ha
+					sido durante mucho tiempo una de las soluciones más confiables en la lucha contra estas amenazas. En este
+					artículo, exploraremos en profundidad Kaspersky Antivirus y por qué es una elección popular en el mundo de la
+					seguridad informática.
+				</p>
+
+				<p className="text-lg font-bold">La Historia de Kaspersky Lab</p>
+
+				<p>
+					Kaspersky Lab fue fundada en 1997 por Eugene Kaspersky, un experto en seguridad informática. Desde sus
+					humildes comienzos, la empresa ha crecido y se ha convertido en un líder mundial en la lucha contra el
+					cibercrimen. Su enfoque en la investigación y el desarrollo de tecnologías de seguridad avanzadas ha ayudado a
+					proteger a millones de usuarios en todo el mundo.
+				</p>
+
+				<p className="text-2xl font-bold">
+					Conoce mas de <span className="text-[#FE3051]">Kaspersky</span>
+				</p>
+				<p>Conoce las ventajas, desventajas y el porque elegir karspersky y descubrela</p>
+				<div className="flex container justify-between gap-5">
+					<Card
+						title={'Ventajas de Kaspersky'}
+						description={
+							'Rendimiento eficiente, firewall y protección de identidad, interfaz intuitiva'
+						}
+					/>
+					<Card
+						title={'Desventajas de Kaspersky'}
+						description={
+							'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed efficitur, lorem sed congue pretium, nisi erat tempus mi, nec porta mi lorem vitae augue.'
+						}
+					/>
+					<Card
+						title={'¿Porque elegir Karspersky?'}
+						description={
+							'Es un sistema de seguridad informatica diseñada para proteger las computadoras o dispositivos electronicos de posibles ataques de softwares maliciosos.'
+						}
+					/>
+				</div>
+
+				<a href="https://acortar.link/mhr1oi" target="blank" className="btn bg-[#FE3051] w-full my-10">
+						Comprar
+					</a>
+			</div>
+		</div>
+	)
 }
