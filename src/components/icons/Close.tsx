ab78d96c1dfd37abdc78e651dbd5d6e2788bcd2f
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2023-11-19 21:42:58 -0600

    Fix stroke-width attribute in Close and Menu icons

--- src/components/icons/Close.tsx
+++ src/components/icons/Close.tsx
@@ -3,12 +3,12 @@ export const Close: React.FC = () => {
 		<svg
 			className="w-6 h-6"
 			fill="none"
-			stroke-width="2"
+			strokeWidth="2"
 			stroke="currentColor"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 		>
-			<path d="M6 18L18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+			<path d="M6 18L18 6M6 6l12 12" strokeLinecap="round" strokeLinejoin="round" />
 		</svg>
 	)
 }
--- src/components/icons/Menu.tsx
+++ src/components/icons/Menu.tsx
@@ -3,12 +3,12 @@ export const Menu: React.FC = () => {
 		<svg
 			className="w-6 h-6"
 			fill="none"
-			stroke-width="2"
+			strokeWidth="2"
 			stroke="currentColor"
 			viewBox="0 0 24 24"
 			xmlns="http://www.w3.org/2000/svg"
 		>
-			<path d="M4 8h16M4 16h16" stroke-linecap="round" stroke-linejoin="round" />
+			<path d="M4 8h16M4 16h16" strokeLinecap="round" strokeLinejoin="round" />
 		</svg>
 	)
 }
